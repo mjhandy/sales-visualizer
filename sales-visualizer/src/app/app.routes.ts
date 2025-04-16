86786ee1cd037afefd6155722312cb5139d13371
Mark J. Handy <mjhandy@outlook.com>
2025-04-16 07:45:14 -0300

    added pages and routing for mader's kitchen

--- sales-visualizer/src/app/app.routes.ts
+++ sales-visualizer/src/app/app.routes.ts
@@ -15,4 +15,22 @@ export const routes: Routes = [
       icon: 'home',
     }  
   },
+  {
+    path: 'maders',    
+    loadComponent: () => import('./comps/pages/maders/landing/landing.component').then((d) => d.MadersLandingComponent),
+    title: "Mader's Kitchen",
+    data:{      
+      label: "Mader's Kitchen",
+      icon: 'home',
+    }  
+  },
+  {
+    path: 'maders/chart',    
+    loadComponent: () => import('./comps/pages/maders/chart/chart.component').then((d) => d.MadersChartComponent),
+    title: "Mader's Kitchen",
+    data:{      
+      label: "Mader's Kitchen",
+      icon: 'home',
+    }  
+  },
 ];
